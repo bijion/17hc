6bc39a7eaf76f501f192d689b80ebfe9d3ac3275
bijion <bijion@gmail.com>
2016-02-20 19:26:07 +0800

    first commit

---
+++ hongcheng/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hongcheng.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InfoList()
+        {
+            return View();
+        }
+    }
+}
---
+++ hongcheng/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hongcheng.MvcApplication" Language="C#" %>
